637db89630fdaafc8f4857265802a3769c573fa0
Riman Farhood <riman.farhood@gmail.com>
2024-01-06 19:44:51 +0000

    Register CarReviewModel in admin.py

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CarReviewModel
 
-# Register your models here.
+
+@admin.register(CarReviewModel)
+class CarReviewModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in CarReviewModel._meta.fields]
+    search_fields = [
+        'make',
+        'model',
+        'year'
+    ]
+    list_filter = ('user',)
