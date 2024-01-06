ef30552d40264277ec1148024c2e69728fde0050
Riman Farhood <riman.farhood@gmail.com>
2024-01-06 19:49:57 +0000

    Setup djrichtextfield editor config in settings.py, update main urls.py

--- main/settings.py
+++ main/settings.py
@@ -68,6 +68,17 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+DJRICHTEXTFIELD_CONFIG = {
+    'js': ['//cdn.ckeditor.com/4.14.0/standard/ckeditor.js'],
+    'init_template': 'djrichtextfield/init/ckeditor.js',
+    'settings': [
+        ['Format', 'Bold', 'Italic', 'Underline'],
+        ['NumberedList', 'BulletedList'], ['Undo', 'Redo'],
+        ['Maximize']
+    ],
+    'format_tags': 'p;h1;h2;h3'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
