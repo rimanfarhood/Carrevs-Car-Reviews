b7d5604c0be2ff35b6e4ea172bbb4632b716da00
Riman Farhood <riman.farhood@gmail.com>
2024-01-07 16:36:23 +0000

    Update home icon, create listview for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -6,8 +6,30 @@ from django.views.generic import (
 from django.contrib.auth.mixins import (
     LoginRequiredMixin
 )
+from django.db.models import Q
 from .models import CarReviewModel
 from .forms import CarReviewModelForm
+
+
+class Reviews(ListView):
+    """View all Reviews"""
+
+    template_name = "reviews/reviews.html"
+    model = CarReviewModel
+    context_object_name = "reviews"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            reviews = self.model.objects.filter(
+                Q(manufacturer__icontains=query) |
+                Q(model__icontains=query) |
+                Q(condition__icontains=query) |
+                Q(year__icontains=query)
+            )
+        else:
+            reviews = self.model.objects.all()
+        return reviews
 
 
 class AddCarReview(LoginRequiredMixin, CreateView):
