98ea29dbb9aff0f3e9b3b19c02e606ec228126bd
Riman Farhood <riman.farhood@gmail.com>
2024-01-07 00:05:11 +0000

    Create view for addning reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView, ListView,
+    DetailView, DeleteView,
+    UpdateView
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin
+)
+from .models import CarReviewModel
+from .forms import CarReviewModelForm
 
-# Create your views here.
+
+class AddCarReview(LoginRequiredMixin, CreateView):
+    """Add car review view"""
+    template_name = 'reviews/add_review.html'
+    model = CarReviewModel
+    form_class = CarReviewModelForm
+    success_url = '/reviews/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddCarReview, self).form_valid(form)
