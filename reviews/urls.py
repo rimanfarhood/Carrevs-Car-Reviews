af56555e7ba2db4fe00c4de01ae5bc26be720cc7
Riman Farhood <riman.farhood@gmail.com>
2024-01-07 16:48:05 +0000

    Create reviews.html, update urls in urls.py and navbar

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddCarReview
+from .views import AddCarReview, Reviews
 
 urlpatterns = [
     path("", AddCarReview.as_view(), name="add_review"),
+    path("reviews/", Reviews.as_view(), name="reviews"),
 ]
