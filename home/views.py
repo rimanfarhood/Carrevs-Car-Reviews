659bd5994bdf2c9ac6998ca856db0871ae2020db
Riman Farhood <riman.farhood@gmail.com>
2024-01-08 01:44:47 +0000

    Complete creation and styling of home page. Add if statment to the 'additional' field in detail page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from reviews.models import CarReviewModel
 
 
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'home/index.html'
+    model = CarReviewModel
+    context_object_name = 'reviews'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
